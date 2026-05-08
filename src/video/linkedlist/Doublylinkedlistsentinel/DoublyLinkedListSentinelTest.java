6bb2ca6930d3906e697a814a000ed5890ed7f852
LM <wycalxy@outlook.com>
2026-05-08 14:29:10 +0800

    test(video): 添加双向链表哨兵测试用例
    - 创建了 DoublyLinkedListSentinelTest 测试类
    - 添加了基本功能测试方法 test1 验证增删遍历操作
    - 添加了边界条件测试方法 test2 验证空链表删除操作
    - 实现了迭代器遍历功能的测试验证
    - 覆盖了链表末尾插入和按索引删除的核心功能测试

---
+++ src/video/linkedlist/Doublylinkedlistsentinel/DoublyLinkedListSentinelTest.java
@@ -0,0 +1,36 @@
+package video.linkedlist.Doublylinkedlistsentinel;
+
+import org.junit.jupiter.api.Test;
+
+public class DoublyLinkedListSentinelTest {
+    @Test
+    public void test1() {
+        DoublyLinkedListSentinel d = new DoublyLinkedListSentinel();
+        d.addLast(1);
+        d.addLast(2);
+        d.addLast(3);
+        d.addLast(4);
+
+        for (Integer value : d) {
+            System.out.println(value);
+        }
+
+        System.out.println("---------------");
+
+        d.remove(0);
+        for (Integer value : d) {
+            System.out.println(value);
+        }
+    }
+
+    @Test
+    public void test2() {
+        DoublyLinkedListSentinel d = new DoublyLinkedListSentinel();
+
+        d.removeLast();
+
+        for (Integer value : d) {
+            System.out.println(value);
+        }
+    }
+}
