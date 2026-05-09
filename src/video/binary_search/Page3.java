a4b7b80adf175534193cd0f70dc592c7fe560b02
LM <wycalxy@outlook.com>
2026-05-09 23:02:49 +0800

    style(video): 格式化二分查找代码样式
    - 统一数组初始化中的空格格式
    - 规范运算符周围的空格间距
    - 整理条件语句中的括号和空格格式
    - 统一日志输出语句的空格格式

--- src/video/binary_search/Page3.java
+++ src/video/binary_search/Page3.java
@@ -3,14 +3,14 @@ package video.binary_search;
 import org.junit.jupiter.api.Test;
 
 /*
-* 二分查找（第三版）
-* 1，2，3版中3最快
-* */
+ * 二分查找（第三版）
+ * 1，2，3版中3最快
+ * */
 
 public class Page3 {
     @Test
     public void test() {
-        int[] arr = {1, 2, 3,4,5,6,7,8,9,10
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 //                ,11,12,13,14,15,16,17,18,19,20
 //                ,21,22,23,24,25,26,27,28,29,30
 //                ,31,32,33,34,35,36,37,38,39,40
@@ -21,7 +21,7 @@ public class Page3 {
 //                ,81,82,83,84,85,86,87,88,89,90
 //                ,91,92,93,94,95,96,97,98,99,100
         };
-        int i=binarySearch(arr, 10);
+        int i = binarySearch(arr, 10);
         System.out.println(i);
     }
 
@@ -30,20 +30,20 @@ public class Page3 {
         int right = arr.length - 1;
         int num = 0;
 
-        while (1 < right-left) {
+        while (1 < right - left) {
             num++;
             int m = (left + right) / 2;
-            if(target<arr[m]){
+            if (target < arr[m]) {
                 right = m;
-            }else {
+            } else {
                 left = m;
             }
         }
-        if(arr[left]==target){
-            System.out.println("num:"+num);
+        if (arr[left] == target) {
+            System.out.println("num:" + num);
             return left;
-        }else {
-            System.out.println("num:"+num);
+        } else {
+            System.out.println("num:" + num);
             return -1;
         }
     }
