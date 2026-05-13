fd8f935d92fbf5369c6ca4596304a2cec3550814
LM <wycalxy@outlook.com>
2026-05-13 23:10:22 +0800

    refactor(leetcode): 将链表反转方法改为递归实现
    - 移除原有的迭代方式实现
    - 添加递归终止条件判断
    - 实现递归调用反转链表逻辑
    - 更新方法注释说明为递归实现

--- src/leetcode/_206/Method3.java
+++ src/leetcode/_206/Method3.java
@@ -3,20 +3,18 @@ package leetcode._206;
 import leetcode.ListNode;
 
 /*
- * 方法2，没有新的节点，只改变指针指向
+ * 递归
  * */
 
 public class Method3 {
     public ListNode reverseList(ListNode head) {
-        ListNode n1 = null;
-        ListNode n2 = head;
-
-        while (n2 != null){
-            ListNode n3 = n2.next;
-            n2.next = n1;
-            n1 = n2;
-            n2 = n3;
+        if (head == null || head.next == null) {
+            return head;
         }
-        return n1;
+
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
     }
 }
