f1d050aafa2544d51bd6679e2b8e005f229a61af
LM <wycalxy@outlook.com>
2026-05-13 22:56:14 +0800

    feat(leetcode): 添加反转链表方法三的实现
    - 实现了不创建新节点的链表反转算法
    - 使用三个指针变量进行链表遍历和指针重定向
    - 完成了核心反转逻辑的while循环结构
    - 返回反转后的新头节点

---
+++ src/leetcode/_206/Method3.java
@@ -0,0 +1,22 @@
+package leetcode._206;
+
+import leetcode.ListNode;
+
+/*
+ * 方法2，没有新的节点，只改变指针指向
+ * */
+
+public class Method3 {
+    public ListNode reverseList(ListNode head) {
+        ListNode n1 = null;
+        ListNode n2 = head;
+
+        while (n2 != null){
+            ListNode n3 = n2.next;
+            n2.next = n1;
+            n1 = n2;
+            n2 = n3;
+        }
+        return n1;
+    }
+}
