28ce41ae2342cb3c08fe465498449b31983ce9d5
LM <wycalxy@outlook.com>
2026-05-16 21:21:53 +0800

    refactor(queue): 优化ArrayQueue3类的接口实现
    - 移除多余的Collection相关导入和未使用的Queue接口
    - 删除未正确实现的size、contains、toArray等方法
    - 移除未实现的add、remove、containsAll等集合操作方法
    - 清理未使用的element和remove方法
    - 简化类的整体结构并专注于队列核心功能

--- src/video/queue/arrayqueue/ArrayQueue3.java
+++ src/video/queue/arrayqueue/ArrayQueue3.java
@@ -1,8 +1,9 @@
 package video.queue.arrayqueue;
 
-import java.util.Collection;
+import video.queue.Queue;
+
 import java.util.Iterator;
-import java.util.Queue;
+
 
 public class ArrayQueue3<E> implements Queue<E>, Iterable<E> {
 
@@ -15,22 +16,12 @@ public class ArrayQueue3<E> implements Queue<E>, Iterable<E> {
     }
 
     @Override
-    public int size() {
-        return 0;
-    }
-
-    @Override
     public boolean isEmpty() {
         return head == tail;
     }
 
     public boolean isFull() {
         return tail - head == array.length;
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return false;
     }
 
     @Override
@@ -53,51 +44,6 @@ public class ArrayQueue3<E> implements Queue<E>, Iterable<E> {
     }
 
     @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return null;
-    }
-
-    @Override
-    public boolean add(E e) {
-        return false;
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends E> c) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public void clear() {
-
-    }
-
-    @Override
     public boolean offer(E e) {
         if (isFull()) {
             return false;
@@ -105,11 +51,6 @@ public class ArrayQueue3<E> implements Queue<E>, Iterable<E> {
         array[(int) (Integer.toUnsignedLong(tail) % array.length)] = e;
         tail++;
         return true;
-    }
-
-    @Override
-    public E remove() {
-        return null;
     }
 
     @Override
@@ -121,11 +62,6 @@ public class ArrayQueue3<E> implements Queue<E>, Iterable<E> {
         array[head % array.length] = null;
         head++;
         return value;
-    }
-
-    @Override
-    public E element() {
-        return null;
     }
 
     @Override
