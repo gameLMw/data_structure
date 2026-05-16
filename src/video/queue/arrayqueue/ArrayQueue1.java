f817a412e5488b159d144ea3b5df18647c3e5ae8
LM <wycalxy@outlook.com>
2026-05-16 21:22:40 +0800

    refactor(video): 优化环形数组队列实现
    - 移除未使用的 Collection 和 Queue 导入
    - 删除空的重写方法实现，包括 size、contains、toArray、add、remove 等
    - 清理冗余的 import 语句顺序
    - 保留核心队列操作方法如 offer、poll、peek 等功能实现

--- src/video/queue/arrayqueue/ArrayQueue1.java
+++ src/video/queue/arrayqueue/ArrayQueue1.java
@@ -1,8 +1,9 @@
 package video.queue.arrayqueue;
 
-import java.util.Collection;
+import video.queue.Queue;
+
 import java.util.Iterator;
-import java.util.Queue;
+
 
 /*
  * 环形数组实现队列1
@@ -20,22 +21,12 @@ public class ArrayQueue1<E> implements Queue<E>, Iterable<E> {
     }
 
     @Override
-    public int size() {
-        return 0;
-    }
-
-    @Override
     public boolean isEmpty() {
         return head == tail;
     }
 
     public boolean isFull() {
         return (tail + 1) % array.length == head;
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return false;
     }
 
     @Override
@@ -58,51 +49,6 @@ public class ArrayQueue1<E> implements Queue<E>, Iterable<E> {
     }
 
     @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return null;
-    }
-
-    @Override
-    public boolean add(E e) {
-        return false;
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends E> c) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        return false;
-    }
-
-    @Override
-    public void clear() {
-
-    }
-
-    @Override
     public boolean offer(E e) {
         if (isFull()) {
             return false;
@@ -110,11 +56,6 @@ public class ArrayQueue1<E> implements Queue<E>, Iterable<E> {
         array[tail] = e;
         tail = (tail + 1) % array.length;
         return true;
-    }
-
-    @Override
-    public E remove() {
-        return null;
     }
 
     @Override
@@ -126,11 +67,6 @@ public class ArrayQueue1<E> implements Queue<E>, Iterable<E> {
         array[head] = null;
         head = (head + 1) % array.length;
         return value;
-    }
-
-    @Override
-    public E element() {
-        return null;
     }
 
     @Override
