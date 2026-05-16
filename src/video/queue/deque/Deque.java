a38ab37c362b5a346f50b3f824807928f8068c65
LM <wycalxy@outlook.com>
2026-05-16 21:21:27 +0800

    feat(video): 添加双端队列接口定义
    - 定义了 Deque 接口的基本操作方法
    - 包含首尾插入、删除、查看元素的方法
    - 添加了队列状态检查方法 isEmpty 和 ifFull

---
+++ src/video/queue/deque/Deque.java
@@ -0,0 +1,20 @@
+package video.queue.deque;
+
+public interface Deque<E> {
+
+    boolean offerFirst(E  e);
+
+    boolean offerLast(E e);
+
+    E pollFirst();
+
+    E pollLast();
+
+    E peekFirst();
+
+    E peekLast();
+
+    boolean isEmpty();
+
+    boolean ifFull();
+}
